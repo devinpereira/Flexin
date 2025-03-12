c32b84b14f47303d6b12e534f1c68d57bf7c0983
Nipuna Lakruwan <laknipsa@gmail.com>
2025-03-13 03:07:00 +0530

    Update Navbar styles and links for improved navigation

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
+
 const Navbar = () => {
   return (
     <nav className="bg-black/30 rounded-[15px] backdrop-blur-[10.25px] p-2 mx-30 mt-15">
       <div className="container mx-auto flex items-center justify-between">
-        {/* /* Left side - Logo */}
+        {/* Left side - Logo */}
         <div className="flex items-center">
           <img src="/logo.svg" alt="Logo" width="120" height="40" className='cursor-pointer' />
           <div className="hidden md:flex space-x-15 ml-20">
-            <a href="#" className="text-white hover:text-gray-300 text-lg">
+            <a href="#" className="text-white hover:text-[#f67a45] text-lg">
               HOME
             </a>
-            <a href="/Store.jsx" className="text-white hover:text-gray-300 text-lg">
+            <a href="/store" className="text-white hover:text-[#f67a45] text-lg">
               STORE
             </a>
-            <a href="#" className="text-white hover:text-gray-300 text-lg">
+            <a href="#" className="text-white hover:text-[#f67a45] text-lg">
               CALCULATORS
             </a>
-            <a href="#" className="text-white hover:text-gray-300 text-lg">
+            <a href="#" className="text-white hover:text-[#f67a45] text-lg">
               TRAINERS
             </a>
-            <a href="#" className="text-white hover:text-gray-300 text-lg">
+            <a href="#" className="text-white hover:text-[#f67a45] text-lg">
               COMMUNITY
             </a>
           </div>
