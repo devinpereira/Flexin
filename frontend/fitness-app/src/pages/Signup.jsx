40a54b3c52a920781b758eb4bfe46401c2dbfd73
Pasindu Fdo <deshanfdo27@gmail.com>
2025-03-25 16:24:40 +0530

    .

--- frontend/fitness-app/src/pages/Signup.jsx
+++ frontend/fitness-app/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Navigation from '../components/Navigation';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion'; // Add framer-motion for animations
