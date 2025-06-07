4e7524cae43eca2893fcbff331866adf7c6be236
Pasindu Fdo <deshanfdo27@gmail.com>
2025-06-07 20:03:26 +0530

    google auth integration

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
+import passport from "passport";
+import jwt from "jsonwebtoken";
 import { protect } from "../middleware/authMiddleware.js";
 import { registerUser, loginUser, getUserInfo } from "../controllers/authController.js";
+import { googleAuthCallback } from "../controllers/oauthController.js";
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
 router.get("/getUserInfo", protect, getUserInfo);
+
+// Google OAuth
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false, failureRedirect: "/login" }),
+  googleAuthCallback
+);
 
 export default router;
